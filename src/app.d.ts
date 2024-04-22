af588bd43b048009be09d939ecda7116c620679d
Adam Rackis <adamrackis@hotmail.com>
2024-04-22 10:26:05 -0400

    Add typings

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,13 @@
+import { DefaultSession } from '@auth/core/types';
+
+declare module '@auth/core/types' {
+	interface Session {
+		userId: string;
+		legacySync: boolean;
+		user: DefaultSession['user'];
+	}
+}
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
@@ -8,5 +18,3 @@ declare global {
 		// interface Platform {}
 	}
 }
-
-export {};
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,7 +2,12 @@ export const load = async ({ locals }) => {
 	const session = await locals.getSession();
 	const loggedIn = !!session?.user;
 
+	const legacySync = session.legacySync;
+	const userId = session.userId;
+
 	return {
-		loggedIn
+		loggedIn,
+		userId,
+		legacySync
 	};
 };
